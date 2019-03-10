5cc46055c4aa1eee65ee180d52acf21ea10965fb
ZenonWow <ZenonWow@mail.com>
2019-03-10 02:43:44 +0100

    add AceEvent:AceEvent:SendMockEvent(eventName, ...)  , separate CallbackHandler.DispatchDynamic()

---
+++ AceEvent-3.0/AceEvent.SendMockEvent.lua
@@ -0,0 +1,73 @@
+local AceEvent = LibStub:GetLibrary("AceEvent-3.0")
+
+if not AceEvent then return end
+
+
+
+function AceEvent:SendMockEvent(eventName, ...)
+	local frames = { G.GetFramesRegisteredForEvent(eventName) }
+	if  not next(frames)  then  return 0  end
+
+	local wrapper = LibShared.GetSafecallWrapper(eventName, ...)
+	local callbacksRan = 0
+	local errorhandler = LibShared.errorhandler
+	-- local errorhandler = G.geterrorhandler()
+
+	for i,frame in  ipairs(frames)  do
+		local callback = frame:GetScript('OnEvent')
+		if callback then
+			local closure = wrapper(callback, frame)
+			local ok = xpcall(closure, errorhandler)
+			if ok then  callbacksRan = callbacksRan + 1  end
+		end
+	end
+	return  callbacksRan
+end
+
+
+
+--[[
+function AceEvent:SendMockEvent(eventName, ...)
+	-- Yet another DispatchDynamic(), now with frames and OnEvent script.
+	-- Differences:  source of receiver list,  source of receiver -> function mapping
+
+	local frames = { G.GetFramesRegisteredForEvent(eventName) }
+	if  not next(frames)  then  return 0  end
+
+	local argNum = select('#',...)
+	local methodClosure, selfArg, callback
+	if  0 == argNum  then
+		methodClosure = function()  callback( selfArg, eventName )  end
+	elseif  1 == argNum  then
+		local arg = ...
+		methodClosure = function()  callback( selfArg, eventName, arg )  end
+	else
+		-- Pack the parameters in a closure to pass to the actual function.
+		local args = { eventName, ... }
+		-- Unpack the parameters in the closure.
+		-- xpcallClosure = function()  callback( unpack(args,1,argNum) )  end
+		methodClosure = function()  callback( selfArg, unpack(args,1,argNum) )  end
+	end
+
+	local callbacksRan = 0
+	local errorhandler = LibShared.errorhandler
+	-- local errorhandler = G.geterrorhandler()
+	for i,frame in ipairs(frames) do
+		selfArg = frame
+		callback = frame:GetScript('OnEvent')
+		local ok = callback and xpcall(methodClosure, errorhandler)
+		if ok then  callbacksRan = callbacksRan + 1  end
+	end
+	return  callbacksRan
+end
+--]]
+
+
+--[[
+/dump  (function()  local l={GetFramesRegisteredForEvent( 'ADDON_LOADED' )} ; for i,f in ipairs(l) do  l[i] = f:GetName()  end ; return l end)()
+/dump  (function()  local l={GetFramesRegisteredForEvent( 'PLAYER_LOGOUT' )} ; for i,f in ipairs(l) do  l[i] = f:GetName()  end ; return l end)()
+/run  AceEvent30Frame:RegisterEvent('MadeUpEvent')
+/dump  (function()  local evt= 'MadeUpEvent'  ;  AceEvent30Frame:RegisterEvent(evt)  ;  local l={GetFramesRegisteredForEvent(evt)} ; for i,f in ipairs(l) do  l[i] = f:GetName()  end ; return l end)()
+/dump  (function()  local evt= 'PLAYER_LOGOUT'  ;  local l={GetFramesRegisteredForEvent(evt)} ; for i,f in ipairs(l) do  l[i] = f:GetName()  end ; l[#l+1]="no?" ; return l end)()
+--]]
+
---
+++ CallbackHandler-1.0/CallbackHandler.DispatchDynamic.lua
@@ -0,0 +1,48 @@
+if not CallbackHandler.DispatchDynamic then
+
+	local next,type,select,unpack,xpcall = next,type,select,unpack,xpcall
+
+	------------------------------
+	-- CallbackHandler. DispatchDynamic(callbacks, ...)
+	--
+	-- This is a version of LibDispatch.CallWrapperDynamic(), with iteration of callbacks and xpcall() added.
+	--
+	function CallbackHandler.DispatchDynamic(callbacks, ...)
+		if  not next(callbacks)  then  return 0  end
+
+		local callback, selfArg
+		local functionClosure, methodClosure
+		local argNum = select('#',...)
+
+		if  0 == argNum  then
+			methodClosure   = function()  callback(selfArg)  end
+		elseif  1 == argNum  then
+			local arg1 = ...
+			functionClosure = function()  callback(arg1)  end
+			methodClosure   = function()  callback(selfArg, arg1)  end
+		else
+			-- Pack the parameters into an array.
+			local args = {...}
+			-- Unpack the parameters in the closure.
+			functionClosure = function()  callback( unpack(args,1,argNum) )  end
+			methodClosure   = function()  callback( selfArg, unpack(args,1,argNum) )  end
+		end
+
+		local callbacksRan,closure = 0
+		-- local errorhandler = _G.geterrorhandler()
+		for  receiver,method  in  next, callbacks  do
+			selfArg = receiver
+			if type(method) == 'function' then
+				callback,closure = method, functionClosure
+			else
+				callback,closure = receiver[method], methodClosure
+			end
+			local ok = xpcall(closure, errorhandler)
+			if ok then  callbacksRan = callbacksRan + 1  end
+		end
+		return callbacksRan
+	end
+
+end -- CallbackHandler.DispatchDynamic
+
+
